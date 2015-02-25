736520c78f212231443b354280db551680d805d2
Michael Compton <guthur@hotmail.com>
2015-02-25 23:51:57 +1030

    Added order matching and order list utilities Improved test file wording and added more tests for utility functions Changed order and order book structures to hash maps for cleaner access

--- src/market_simulation/core.clj
+++ src/market_simulation/core.clj
@@ -1,7 +1,6 @@
 (ns market-simulation.core
   (:gen-class)
-  (:require [clojure.core.reducers :as r]
-            [clojure.test.check :as tc]
+  (:require [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clj-time.core :as t]))
@@ -13,48 +12,26 @@
 
 (defn make-order 
   "Make an order of QUANTITY at PRICE."
-  [price quantity]
-  (list price quantity (t/now)))
-
-(defn order-price 
-  "Get ORDER price"
-  [order]
-  (first order))
-
-(defn order-quantity 
-  "Get ORDER quantity"
-  [order]
-  (second order))
-
-(defn order-time [order]
-  "Get ORDER time"
-  (nth order 2))
+  ([price quantity]
+   (make-order price quantity (t/now)))
+  ([price quantity time]
+   {:price price :quantity quantity :time time}))
 
 (defn make-order-book 
   "Make an order book. If 0 arguments are passed the order book is
   empty. Buy and sell orders can be passed via BUYS and SELLS
   respectively"
-  ([] (list nil nil))
+  ([] (make-order-book () ()))
   ([buys sells]
-   (list buys sells)))
-
-(defn sell-orders 
-  "Get sell orders for ORDER-BOOK"
-  [order-book]
-  (second order-book))
-
-(defn buy-orders 
-  "Get buy orders for ORDER-BOOK"
-  [order-book]
-  (first order-book))
+   {:buy-orders buys :sell-orders sells}))
 
 (defn make-order-comparator
   "Return a function that compares two orders using the
   PRICE-COMPARATOR for price and `t/before?' for order time"
   [price-comparator]
-  (fn [x y]
-    (or (price-comparator (order-price x) (order-price y))
-        (and (= (order-price x) (order-price y)) (t/before? (order-time x) (order-time y))))))
+  (fn [x y]    
+    (or (price-comparator (:price x) (:price y))
+        (and (= (:price x) (:price y)) (t/before? (:time x) (:time y))))))
 
 (defn sort-order-list 
   "Add ORDER to ORDER-LIST and sort order using PRICE-COMPARATOR"
@@ -66,13 +43,98 @@
   order book. The order list is sorted in descending price order and
   most recent"
   [order-book order]
-  (make-order-book (sort-order-list order (buy-orders order-book) >)
-                   (sell-orders order-book)))
+  (make-order-book (sort-order-list order (:buy-orders order-book) >)
+                   (:sell-orders order-book)))
 
 (defn add-sell-order 
   "Add ORDER to ORDER-BOOK sell orders sorted list returning the new
   order book. The order list is sorted in ascending price order and
   most recent"
   [order-book order]
-  (make-order-book (buy-orders order-book)
-                   (sort-order-list order (sell-orders order-book) <)))
+  (make-order-book (:buy-orders order-book)
+                   (sort-order-list order (:sell-orders order-book) <)))
+
+(defn get-order-list-top-price 
+  "Get the price of the top order in ORDER-LIST"
+  [order-list]
+  (:price (first order-list)))
+
+(defn get-top-sell-price 
+  "Get top sell price in ORDER-BOOK"
+  [order-book]
+  (get-order-list-top-price (:sell-orders order-book)))
+
+(defn get-top-buy-price
+  "Get top buy price in ORDER-BOOK"
+  [order-book]
+  (get-order-list-top-price (:buy-orders order-book)))
+
+(defn get-order-list-quantity
+  "Get the quantity sum of all the orders in ORDER-LIST"
+  [order-list]
+  (reduce (fn [count order] (+ count (:quantity order))) 0 order-list))
+
+(defn get-sell-list-quantity
+  "Get the quantity sum of all the sell orders in ORDER-BOOK"
+  [order-book]
+  (get-order-list-quantity (:sell-orders order-book)))
+
+(defn get-buy-list-quantity 
+  "Get the quantity sum of all the buy orders in ORDER-BOOK"
+  [order-book]
+  (get-order-list-quantity (:buy-orders order-book)))
+
+(defn fill-order [order order-list matching-price-comparator]
+  "Recurse through ORDER-LIST searching for matching prices against
+  the ORDER using MATCHING-PRICE-COMPARATOR. Returning remaining ORDER
+  and ORDER-LIST"
+  (let [top-of-book (first order-list)]
+    (cond
+      ;; Order has zero quantity return remaining ORDER-LIST
+      (zero? (:quantity order))
+      [nil order-list]
+      ;; Empty ORDER-LIST return ORDER and ORDER-LIST
+      (empty? order-list)
+      [order order-list]
+      ;; Price can not close on any existing orders in ORDER-LIST
+      ;; return ORDER-LIST and ORDER
+      (not (matching-price-comparator (:price order) (:price top-of-book)))
+      [order order-list]
+      ;; Price matches. If quantity of ORDER is greater or equal to
+      ;; top-of-book then remove top-of-book quantity from ORDER and
+      ;; recursively call `FILL-ORDER with the remaining ORDER
+      ;; quantity and remaining ORDER-LIST'
+      (>= (:quantity order) (:quantity top-of-book))
+      (fill-order (make-order (:price order)
+                              (- (:quantity order)
+                                 (:quantity top-of-book))
+                              (:time order))
+                  (rest order-list)
+                  matching-price-comparator)
+      ;; Price matches and ORDER quantity less than top-of-book,
+      ;; remove ORDER quantity from top-of-book quantity returning new
+      ;; ORDER-LIST
+      :else
+      [nil (cons (make-order (:price top-of-book)
+                             (- (:quantity top-of-book)
+                                (:quantity order))
+                             (:time top-of-book))
+                 (rest order-list))])))
+
+(defn match-sell-order [order order-book]
+  "Tries to match sell ORDER against any buy orders in ORDER-BOOK when
+  the sell price is less than or equal to the buy price. Any
+  unfulfilled sell ORDER is added to the ORDER-BOOK"
+  (let [[order order-list] (fill-order order (:buy-orders order-book) <=)]
+    (if (= nil order)
+      (make-order-book order-list (:sell-orders order-book))
+      (add-sell-order (make-order-book order-list (:sell-orders order-book)) order))))
+
+(defn match-buy-order [order order-book]
+  "Tries to match buy ORDER against any sell orders in ORDER-BOOK when
+  the buy price is greater than or equal to the sell price. Any
+  unfulfilled buy ORDER is added to the ORDER-BOOK"
+  (let [[order order-list] (fill-order order (:sell-orders order-book) >=)]
+    (if (= nil order)
+      (make-order-book (:buy-orders order-book) order-list)
+      (add-buy-order (make-order-book (:buy-orders order-book) order-list) order))))
--- test/market_simulation/core_test.clj
+++ test/market_simulation/core_test.clj
@@ -1,27 +1,26 @@
 (ns market-simulation.core-test
-  (:require [clojure.core.reducers :as r]
-            [clojure.test :refer :all]
+  (:require [clojure.test :refer :all]
             [clojure.test.check :as tc]            
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :as tcct]
             [market-simulation.core :refer :all]))
 
-(def number-of-test-runs 1000)
+(def number-of-check-runs 1000)
 
-(comment "Test the creation of an order. Checking that price and
+(comment "Check the creation of an order. Checking that price and
 quantity are correct and that the time is a DataTime object")
 (tcct/defspec generate-order
-  number-of-test-runs
+  number-of-check-runs
   (prop/for-all [generated-price gen/int
                  generated-quantity gen/int]
                 (let [order (make-order generated-price generated-quantity)]
-                  (and (= generated-price (order-price order))
-                       (= generated-quantity (order-quantity order))
-                       (= org.joda.time.DateTime (type (order-time order)))))))
+                  (and (= generated-price (:price order))
+                       (= generated-quantity (:quantity order))
+                       (= org.joda.time.DateTime (type (:time order)))))))
 
 (defn make-price-comparator-prop
-  "Create test property to test the price ordering for
+  "Create check property to check the price ordering for
   `make-order-comparator' using PRICE-COMPARATOR. Property ignores
   when prices are equal due to non-determinism of order time which is
   in the secondary comparision when prices are equal."
@@ -36,37 +35,80 @@ quantity are correct and that the time is a DataTime object")
                          (price-comparator price-x price-y))
                       (= price-x price-y)))))
 
-(comment "Test price is greater order comparision")
+(comment "Check price is greater order comparision")
 (tcct/defspec price-greater-order-comparator
-  number-of-test-runs
+  number-of-check-runs
   (make-price-comparator-prop >))
 
-(comment "Test price is less than order comparision")
+(comment "Check price is less than order comparision")
 (tcct/defspec price-less-than-order-comparator
-  number-of-test-runs
+  number-of-check-runs
   (make-price-comparator-prop <))
 
-(defn make-add-orders-test-prop 
-  "Create a test property for adding orders. The function to add the
+(defn make-add-orders-check-prop 
+  "Create a check property for adding orders. The function to add the
   particular order type is passed via ADD-ORDER-FN. The order accessor
-  function is passed via ORDER-ACCESSOR, the price comparator is
+  function is passed via ORDER-LIST-ACCESSOR, the price comparator is
   passed via PRICE-COMPARISON."
-  [add-order-fn order-accessor price-comparison]
+  [add-order-fn order-list-accessor price-comparison]
   (prop/for-all [prices-and-quantities (gen/vector (gen/vector gen/pos-int 2))]
-                (let [order-book (r/reduce (fn [book order-values]
-                                             (add-order-fn book (apply make-order order-values)))
-                                           (make-order-book) 
-                                           prices-and-quantities)]
-                  (and (= (count prices-and-quantities) (count (order-accessor order-book)))
+                (let [order-book (reduce (fn [book order-values]
+                                           (add-order-fn book (apply make-order order-values)))
+                                         (make-order-book) 
+                                         prices-and-quantities)]
+                  (and (= (count prices-and-quantities) (count (order-list-accessor order-book)))
                        (= (sort price-comparison (map first prices-and-quantities))
-                          (map order-price (order-accessor order-book)))))))
+                          (map :price (order-list-accessor order-book)))))))
 
-(comment "Test the addition of buy orders to an order-book")
+(comment "Check the addition of buy orders to an order-book")
 (tcct/defspec add-buy-orders
-  number-of-test-runs
-  (make-add-orders-test-prop add-buy-order buy-orders >))
+  number-of-check-runs
+  (make-add-orders-check-prop add-buy-order :buy-orders >))
 
-(comment "Test the addition of sell orders to an order-book")
+(comment "Check the addition of sell orders to an order-book")
 (tcct/defspec add-sell-orders
-  number-of-test-runs
-  (make-add-orders-test-prop add-sell-order sell-orders <))
+  number-of-check-runs
+  (make-add-orders-check-prop add-sell-order :sell-orders <))
+
+(defn make-orders-generator
+  "Return a generator that produces and non-empty list of orders"
+  []
+  (gen/such-that not-empty (gen/vector (gen/fmap (partial apply make-order) 
+                                                 (gen/tuple gen/pos-int gen/pos-int)))))
+
+(defn make-order-book-generator
+  "Return an order book that has orders added using ADD-ORDER-FN"
+  [add-order-fn]
+  (gen/fmap (partial reduce add-order-fn (make-order-book)) (make-orders-generator)))
+
+(comment "Check getting the top sell price of an order book")
+(tcct/defspec check-get-top-sell-price
+  number-of-check-runs
+  (prop/for-all [order-book (make-order-book-generator add-sell-order)]
+                (let [top-sell-price (reduce min (map :price (:sell-orders order-book)))]
+                  (= (get-top-sell-price order-book)
+                     top-sell-price))))
+
+(comment "Check getting the top buy price of an order book")
+(tcct/defspec check-get-top-buy-price
+  number-of-check-runs
+  (prop/for-all [order-book (make-order-book-generator add-buy-order)]
+                (let [top-buy-price (reduce max (map :price (:buy-orders order-book)))]
+                  (= (get-top-buy-price order-book)
+                     top-buy-price))))
+
+(comment "Check getting the total quantity of all buy orders within an order book")
+(tcct/defspec check-get-buy-list-quantity
+  number-of-check-runs
+  (prop/for-all [order-book (make-order-book-generator add-buy-order)]
+                (let [total-quantity (reduce + (map :quantity (:buy-orders order-book)))]
+                  (= (get-buy-list-quantity order-book)
+                     total-quantity))))
+
+(comment "Check getting the total quantity of all sell orders within an order book")
+(tcct/defspec check-get-sell-list-quantity
+  number-of-check-runs
+  (prop/for-all [order-book (make-order-book-generator add-sell-order)]
+                (let [total-quantity (reduce + 0 (map :quantity (:sell-orders order-book)))]
+                  (= (get-sell-list-quantity order-book)
+                     total-quantity))))
